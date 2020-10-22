ef1c182cc9021a433717ca09b1ab2a4fd09e1f86
w55186hs <hanyu.shen@stydent.manchester.ac.uk>
2020-10-22 08:00:18 +0100

    Coursework_o1

---
+++ status.py
@@ -0,0 +1,10 @@
+operand1 = input("Input a number: ")
+print ("You entered " + operand1)
+def printState():
+    print ("Red Light is " + redLight)
+    print ("Yellow Light is " + yellowLight)
+    print ("Green Light is " + greenLight)
+redLight = "on"
+yellowLight = "off"
+greenLight = "off"
+printState()
