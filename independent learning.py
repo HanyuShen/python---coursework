bdbe172c3eed891d41defb69bd5c535069be5be1
w55186hs <hanyu.shen@stydent.manchester.ac.uk>
2020-10-22 08:02:19 +0100

    Coursework_01

---
+++ independent learning.py
@@ -0,0 +1,21 @@
+operand1 = input("PLease enter the first number:")
+print(operand1)
+operand2 = input("Please enter the secong number:")
+print(operand2)
+
+a=operand1
+b=operand2
+c=int(a)+int(b)
+print(c)
+
+d=int(a)*int(b)
+print(d)
+
+e=int(a)/int(b)
+print(e)
+
+f=int(a)%int(b)
+print(f)
+
+g=int(a)**int(b)
+print(g)
